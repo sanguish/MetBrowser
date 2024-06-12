535721d495cfa6a86b3833ee9771dcc2a7a14d43
Scott Anguish <sanguish@me.com>
2024-06-12 01:23:12 -0400

    Initial Project commit
    Adds the original Xcode project code to the barebones Github project

---
+++ MetBrowser/MetBrowser/MetBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetBrowserApp.swift
+//  MetBrowser
+//
+//  Created by Scott Anguish on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MetBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
