5931e3e7b8308d13b5aa9fd007795147dc70fc16
Scott Anguish <sanguish@me.com>
2024-06-19 03:41:22 -0400

    added share sheet
    and selectable text

--- MetBrowser/MetBrowser/Views/MainView/MetArtifactView.swift
+++ MetBrowser/MetBrowser/Views/MainView/MetArtifactView.swift
@@ -50,16 +50,36 @@ struct MetArtifactView: View {
     var metArtifact: MetArtifact
 
     @ViewBuilder
-    func dataLine(label: String, text: String) -> some View {
-        Text("\(label): ").bold() + Text(text)
+    func dataLine(label: String, text: String?) -> some View {
+        Text("**\(label):** \(text ?? "")")
+            .textSelection(.enabled)
     }
+
+    var shareString: String {
+        var outputString = ""
+        outputString = outputString + "Title: \(metArtifact.title)\n"
+        outputString = outputString + "Date: \(metArtifact.objectDate)\n"
+        outputString = outputString + "Culture: \(metArtifact.culture)\n"
+        outputString = outputString + "Medium: \(metArtifact.medium)\n"
+        outputString = outputString + "Classification: \(metArtifact.classification)\n"
+        outputString = outputString + "Credit Line: \(metArtifact.creditLine)\n"
+        outputString = outputString + "Public Domain: \(metArtifact.isPublicDomain ? "Yes" : "No")\n"
+        return outputString
+    }
+
     var body: some View {
         HStack {
             MetArtifactImage(metArtifact: metArtifact)
                 .frame(width: 250)
             VStack(alignment: .leading, spacing: 5) {
-                Text("Overview")
-                    .font(.title)
+                HStack {
+                    Text("Overview")
+                        .font(.title)
+                    Spacer()
+                    ShareLink("", item: shareString, subject: Text(metArtifact.title))
+                        .buttonStyle(PlainButtonStyle())
+                }
+
                 Divider()
                 dataLine(label: "Title", text: metArtifact.title)
                 dataLine(label: "Date", text: metArtifact.objectDate)
